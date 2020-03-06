655c4b9d92edb97d4cf53e12d54ea88b7d1983ee
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2020-03-06 17:48:18 +0545

    added wtf in example

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,18 @@
-import 'package:logger/logger.dart';
 import 'package:logger/src/printers/simple_printer.dart';
+import 'package:logger/logger.dart';
 
 var logger = Logger(
   printer: PrettyPrinter(),
 );
 
 var loggerNoStack = Logger(
-  printer: PrettyPrinter(methodCount: 0),
+  printer: PrettyPrinter(
+    methodCount: 0,
+  ),
 );
 
 void main() {
+  Logger.level = Level.verbose;
   print(
       'Run with either `dart example/main.dart` or `dart --enable-asserts example/main.dart`.');
   demo();
@@ -25,6 +28,8 @@ void demo() {
   logger.e('Error! Something bad happened', 'Test Error');
 
   loggerNoStack.v({'key': 5, 'value': 'something'});
+
+  loggerNoStack.wtf({'key': 5, 'value': 'something'});
 
   Logger(printer: SimplePrinter()..useColor = true).v('boom');
 }
